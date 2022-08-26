514ac029d15f4d6bef74fa2b605da0fa4f38bd87
Mona Lisa <student@bchd.969a8830-1d5d-41d9-b69b-255e20c9c61d>
2022-08-26 20:21:42 +0000

    learning about kafka and more

---
+++ kafka03-consumer.go
@@ -0,0 +1,51 @@
+/* Alta3 Research | RZFeeser
+   Kafka - Consumer talking to kafka broker  */
+
+package main
+
+import (
+        "context"
+        "fmt"
+        "log"
+        "os"
+        "time"
+        "strings"
+
+        kafka "github.com/segmentio/kafka-go"
+)
+
+func getKafkaReader(kafkaURL, topic, groupID string) *kafka.Reader {
+        brokers := strings.Split(kafkaURL, ",")
+        return kafka.NewReader(kafka.ReaderConfig{
+                Brokers:  brokers,
+                GroupID:  groupID,
+                Topic:    topic,
+                MinBytes: 10e3, // 10KB
+                MaxBytes: 10e6, // 10MB
+        })
+}
+
+func main() {
+        // get kafka reader using environment variables.
+        //kafkaURL := os.Getenv("kafkaURL")
+        //topic := os.Getenv("topic")
+        kafkaURL := "localhost:9093"
+        topic := "mymessagelog"
+
+        groupID := os.Getenv("groupID")
+
+        reader := getKafkaReader(kafkaURL, topic, groupID)
+
+        defer reader.Close()
+
+        fmt.Println("start consuming ... !!")
+        for {
+                m, err := reader.ReadMessage(context.Background())
+                if err != nil {
+                        log.Fatalln(err)
+                }
+                fmt.Printf("message at topic:%v partition:%v offset:%v  %s = %s\n", m.Topic, m.Partition, m.Offset, string(m.Key), string(m.Value))
+                time.Sleep(1 * time.Second)
+        }
+}
+
